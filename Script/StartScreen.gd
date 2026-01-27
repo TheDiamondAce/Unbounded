d0bc994f8abecabd8e96e6b7298069fe06a7938b
TheDiamondAce <75910142+TheDiamondAce@users.noreply.github.com>
2026-01-26 20:30:26 -0800

    made video shorter added music temporarily
    The video currently is at 1920x1080p, it is not the best quality. TO FIX IT, you need to implement the 2560x1440p video. edit the video so it has the thing cut out. better yet, add the polished animation.

---
+++ Script/StartScreen.gd
@@ -0,0 +1,33 @@
+extends VideoStreamPlayer
+
+@onready var button =$"../Start"
+@onready var title = $"../tempTitle"
+@onready var level0 =  preload("res://Scene/Level_0.tscn")
+@onready var startScene = $"."
+@onready var auraScene = $"../Sprite2D"
+@onready var startMusic = $"../StartScreenMusic"
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	auraScene.show()
+	startScene.stop()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	button.hide()
+	title.hide()
+	auraScene.hide()
+	startScene.play()
+	startMusic.volume_db == -15
+	
+	pass # Replace with function body.
+
+
+func _on_finished() -> void:
+	get_tree().change_scene_to_packed(level0)
+	pass # Replace with function body.
